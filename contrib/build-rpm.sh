d862aba9bcc9fa8781830ce3d877ea8385b9c59f
Niels Basjes <nbasjes@bol.com>
2017-08-18 13:14:07 +0200

    Improved rpmbuild script

--- contrib/build-rpm.sh
+++ contrib/build-rpm.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
+#
+# A simple script to make building an RPM of the software a lot easier
+#
 
-if [ "$(which rpmbuild)" == "" ];
-then
+set -e
+
+if [ "$(which rpmbuild)" == "" ]; then
   echo "To build an rpm the tool rpmbuild needs to be installed first"
-  exit -1
+  exit 1
 fi 
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ -z $1 ];
-then
+if [[ -z $1 ]]; then
   echo "Usage $0 \"release\""
-  exit -1
+  exit 1
 fi
 
-cd ${DIR}/..
+cd "${DIR}/.." || exit 1
 
-rm -f google-authenticator-*.tar.gz
-./bootstrap.sh && ./configure && make dist &&
+echo "Starting build"
+./bootstrap.sh && \
+./configure && \
+make dist && \
 (
    mkdir -p "${DIR}/_rpmbuild/SOURCES"
-   cp google-authenticator-*.tar.gz "${DIR}/_rpmbuild/SOURCES/"
+   cp -f google-authenticator-*.tar.gz "${DIR}/_rpmbuild/SOURCES/"
    rpmbuild -ba contrib/rpm.spec --define "_topdir ${DIR}/_rpmbuild" --define "_release $1"
 
    echo "=============="
    echo "Available RPMs"
-   find "${DIR}/_rpmbuild/" -type f -name '*.rpm' | fgrep -- "-$1."
-) || echo "Something went wrong"
+   find "${DIR}/_rpmbuild/" -type f -name 'google-authenticator*.rpm' 
+) || echo "Something went wrong" && exit 1
+
+exit 0
+
