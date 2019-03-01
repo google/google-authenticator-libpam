60207b6c4cebf825863043e963bf67f6a0520076
Thomas Habets <habets@google.com>
2019-03-01 14:37:48 +0000

    base32: Include config.h
    Bug #122

--- src/base32_prog.c
+++ src/base32_prog.c
@@ -15,6 +15,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "config.h"
+
 #include <err.h>
 #include <errno.h>
 #include <fcntl.h>
